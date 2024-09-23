cc773284bfd54ea7421ed0f8972371aec06cd04e
TestSeung <sjlim589@hanmail.net>
2024-09-23 10:21:23 +0900

    feature(config): cors 관련 예외처리
    프론트 코드 요청 시 Access-Control-Allow-Origin 관련 예외처리

---
+++ src/main/java/project_1st_team03/dashboard/domain/comment/WebConfig.java
@@ -0,0 +1,17 @@
+package project_1st_team03.dashboard.domain.comment;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//cors 해결 code
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
+    }
+}

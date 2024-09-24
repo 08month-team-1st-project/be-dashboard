0bfe3fd09795655f660dace5491fa0e13e8f2460
DokyungYou <willow74rr@gmail.com>
2024-09-24 20:15:44 +0900

    Feat(회원): 로그인 기능 구현 - #8

--- src/main/java/project_1st_team03/dashboard/domain/member/api/MemberController.java
+++ src/main/java/project_1st_team03/dashboard/domain/member/api/MemberController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project_1st_team03.dashboard.domain.member.application.MemberService;
+import project_1st_team03.dashboard.domain.member.dto.LoginRequest;
+import project_1st_team03.dashboard.domain.member.dto.LoginResponse;
 import project_1st_team03.dashboard.domain.member.dto.SignupRequest;
 
 @RequiredArgsConstructor
@@ -17,14 +19,18 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    // FIXME HttpMessageNotReadableException 계속 발생 중 (일단 그 외엔 정상작동이라서 미뤄둠)
-    // 회원저장도 잘되고(db 에 잘 들어감), 요청값 검증도 잘 되고, 구글링해서 나온 원인들 체크해봤으나 해당사항에 없었음
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody @Valid SignupRequest request) {
 
         memberService.createMember(request);
         return ResponseEntity.ok("회원가입을 축하드립니다.");
     }
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
+        LoginResponse response = memberService.login(request);
+        return ResponseEntity.ok(response);
+    }
+
 
 
 }
--- src/main/java/project_1st_team03/dashboard/domain/member/application/MemberService.java
+++ src/main/java/project_1st_team03/dashboard/domain/member/application/MemberService.java
@@ -6,11 +6,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import project_1st_team03.dashboard.domain.member.dao.MemberRepository;
 import project_1st_team03.dashboard.domain.member.domain.Member;
+import project_1st_team03.dashboard.domain.member.dto.LoginRequest;
+import project_1st_team03.dashboard.domain.member.dto.LoginResponse;
 import project_1st_team03.dashboard.domain.member.dto.SignupRequest;
 import project_1st_team03.dashboard.domain.member.exception.MemberException;
+import project_1st_team03.dashboard.global.security.JwtService;
+import project_1st_team03.dashboard.global.security.MemberDetails;
 import project_1st_team03.dashboard.global.security.MemberRole;
 
-import static project_1st_team03.dashboard.global.exception.ErrorCode.ALREADY_REGISTERED_EMAIL;
+import java.util.HashMap;
+import java.util.Map;
+
+import static project_1st_team03.dashboard.global.exception.ErrorCode.*;
+import static project_1st_team03.dashboard.global.security.MemberRole.*;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -18,18 +26,33 @@ import static project_1st_team03.dashboard.global.exception.ErrorCode.ALREADY_RE
 public class MemberService {
 
     private final MemberRepository memberRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     @Transactional
     public void createMember(SignupRequest request) {
-
         if (memberRepository.findByEmail(request.getEmail()).isPresent()) {
             throw new MemberException(ALREADY_REGISTERED_EMAIL);
         }
         Member member = Member.createMember(request.getEmail(),
-                bCryptPasswordEncoder.encode(request.getPassword()),
-                MemberRole.USER);
+                passwordEncoder.encode(request.getPassword()),
+                USER);
 
         memberRepository.save(member);
+    }
+
+    public LoginResponse login(LoginRequest request) {
+        // 보안 상 어떤 값이 틀렸는지 알 수 없게 하기 위해 같은 에러코드를 사용하였음
+        Member findMember = memberRepository.findByEmail(request.getEmail())
+                .orElseThrow(()-> new MemberException(NOT_CORRECT_ACCOUNT_ID_OR_PASSWORD));
+
+        if (!passwordEncoder.matches(request.getPassword(), findMember.getPassword())) {
+            throw new MemberException(NOT_CORRECT_ACCOUNT_ID_OR_PASSWORD);
+        }
+
+        String accessToken = jwtService.generateToken(Map.of("role", USER),
+                new MemberDetails(findMember));
+
+        return new LoginResponse(findMember.getEmail(), accessToken);
     }
 }

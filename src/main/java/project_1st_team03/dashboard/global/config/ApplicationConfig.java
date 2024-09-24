9564a6669a8c751d454faa892baaae9622b4807b
DokyungYou <willow74rr@gmail.com>
2024-09-24 10:08:40 +0900

    Feat(회원): ApplicationConfig.class 추가 & UserDetailsService 빈 등록 - #7

---
+++ src/main/java/project_1st_team03/dashboard/global/config/ApplicationConfig.java
@@ -0,0 +1,40 @@
+package project_1st_team03.dashboard.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import project_1st_team03.dashboard.domain.member.dao.MemberRepository;
+import project_1st_team03.dashboard.domain.member.domain.Member;
+import project_1st_team03.dashboard.global.security.MemberDetails;
+
+import static project_1st_team03.dashboard.global.exception.ErrorCode.NOT_FOUND_MEMBER;
+
+@RequiredArgsConstructor
+@Configuration
+public class ApplicationConfig {
+
+    private final MemberRepository memberRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                Member findMember = memberRepository.findByEmail(username)
+                        .orElseThrow(() -> new UsernameNotFoundException(NOT_FOUND_MEMBER.getMessage()));
+                return new MemberDetails(findMember);
+            }
+        };
+
+        // before 람다 & after 람다 비교해려고 넣었음
+//        return username -> {
+//            Member findMember = memberRepository.findByEmail(username)
+//                    .orElseThrow(() -> new UsernameNotFoundException(NOT_FOUND_MEMBER.getMessage()));
+//            return new MemberDetails(findMember);
+//        };
+    }
+}

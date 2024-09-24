f3e422cadc81d7b49f9145682939e695549e4d7d
DokyungYou <willow74rr@gmail.com>
2024-09-24 15:02:27 +0900

    Feat(회원): ErrorCode - NOT_FOUND_MEMBER 추가 - #7
    - 9564a666 에 누락된 커밋

--- src/main/java/project_1st_team03/dashboard/global/exception/ErrorCode.java
+++ src/main/java/project_1st_team03/dashboard/global/exception/ErrorCode.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.*;
+
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
 
+    //MemberException
+    NOT_FOUND_MEMBER(NOT_FOUND, "존재하지 않는 회원")
+    ;
     private final HttpStatus httpStatus;
     private final String message;
 }

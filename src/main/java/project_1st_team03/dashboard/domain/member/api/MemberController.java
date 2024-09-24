8b44feb4bbcbc41c140c433b39608e232646cc1a
DokyungYou <willow74rr@gmail.com>
2024-09-24 18:18:29 +0900

    Feat(회원): 회원기능 구현 - #8
    - HttpMessageNotReadableException 계속 발생 중 (일단 그 외엔 정상작동이라서 일정상 후순위로 미뤄둠)

--- src/main/java/project_1st_team03/dashboard/domain/member/api/MemberController.java
+++ src/main/java/project_1st_team03/dashboard/domain/member/api/MemberController.java
@@ -1,7 +1,30 @@
 package project_1st_team03.dashboard.domain.member.api;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import project_1st_team03.dashboard.domain.member.application.MemberService;
+import project_1st_team03.dashboard.domain.member.dto.SignupRequest;
 
+@RequiredArgsConstructor
+@RequestMapping("/api")
 @RestController
 public class MemberController {
+
+    private final MemberService memberService;
+
+    // FIXME HttpMessageNotReadableException 계속 발생 중 (일단 그 외엔 정상작동이라서 미뤄둠)
+    // 회원저장도 잘되고(db 에 잘 들어감), 요청값 검증도 잘 되고, 구글링해서 나온 원인들 체크해봤으나 해당사항에 없었음
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody @Valid SignupRequest request) {
+
+        memberService.createMember(request);
+        return ResponseEntity.ok("회원가입을 축하드립니다.");
+    }
+
+
 }
--- src/main/java/project_1st_team03/dashboard/domain/member/application/MemberService.java
+++ src/main/java/project_1st_team03/dashboard/domain/member/application/MemberService.java
@@ -1,7 +1,35 @@
 package project_1st_team03.dashboard.domain.member.application;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project_1st_team03.dashboard.domain.member.dao.MemberRepository;
+import project_1st_team03.dashboard.domain.member.domain.Member;
+import project_1st_team03.dashboard.domain.member.dto.SignupRequest;
+import project_1st_team03.dashboard.domain.member.exception.MemberException;
+import project_1st_team03.dashboard.global.security.MemberRole;
 
+import static project_1st_team03.dashboard.global.exception.ErrorCode.ALREADY_REGISTERED_EMAIL;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 @Service
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Transactional
+    public void createMember(SignupRequest request) {
+
+        if (memberRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new MemberException(ALREADY_REGISTERED_EMAIL);
+        }
+        Member member = Member.createMember(request.getEmail(),
+                bCryptPasswordEncoder.encode(request.getPassword()),
+                MemberRole.USER);
+
+        memberRepository.save(member);
+    }
 }

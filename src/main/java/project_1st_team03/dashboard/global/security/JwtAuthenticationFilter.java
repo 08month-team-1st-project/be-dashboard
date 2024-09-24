cd633d3815783396f3031c1e27fe993faf2b482e
DokyungYou <willow74rr@gmail.com>
2024-09-24 15:00:34 +0900

    Feat(회원): JwtAuthenticationFilter 예외처리, JwtAuthenticationEntryPoint 구현 - #7
    - JwtAuthenticationEntryPoint: AuthenticationEntryPoint 를 상속하여 구현
    - JwtAuthenticationFilter 에서 발생 한 예외를 request에 담아서 보낸다.
      - 예외 종류 상관없이 Exception 으로 catch 하였음
    - JwtAuthenticationEntryPoint 에서 request 안에 담긴 예외를 꺼낸 후 처리

--- src/main/java/project_1st_team03/dashboard/global/security/JwtAuthenticationFilter.java
+++ src/main/java/project_1st_team03/dashboard/global/security/JwtAuthenticationFilter.java
@@ -19,7 +19,7 @@ import java.io.IOException;
 
 /**
  * 일정의 문제로 구현 간소화
- *  - 토큰재발급, 리프레시 토큰 등의 로직 X
+ * - 토큰재발급, 리프레시 토큰 등의 로직 X
  */
 @Slf4j
 @RequiredArgsConstructor
@@ -36,52 +36,55 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
 
         log.debug("jwt 인증필터 진입");
-        
+
         // 요청 시에 Authorization 이라는 헤더로 jwt 인증 토큰을 전달한 상태
         // 해당 헤더에 담아온 토큰을 꺼낸다
         final String authHeader = request.getHeader("Authorization");
         final String jwtToken;
         final String userEmail;
 
-        // 토큰이 필요하지 않은 api일 경우 로직 처리없이 다음 필터로 이동
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+        try {
+            // 토큰이 필요하지 않은 api일 경우 로직 처리없이 다음 필터로 이동
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            jwtToken = authHeader.substring(7); // Bearer 접두사 제거하여 토큰추출
+            userEmail = jwtService.extractUsername(jwtToken);
+
+
+            //  아직 인증 상태가 아닐때 (null 이면 아직 인증 X)
+            if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail); // db 로부터 사용자정보 로드
+
+                if (jwtService.isTokenValid(jwtToken, userDetails)) {
+
+                    // 인증 객체(Authentication) 얻기
+                    // 참고: UsernamePasswordAuthenticationToken (구현체) -> Authentication
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,   // 현재 구현에서는 자격증명이 없기에 null로 넘김
+                            userDetails.getAuthorities() // 권한 넘김
+                    );
+
+                    // 참고: WebAuthenticationDetailsSource (구현체) -> AuthenticationDetailsSource
+                    authToken.setDetails(
+                            new WebAuthenticationDetailsSource().buildDetails(request) // WebAuthenticationDetails 반환
+                    );
+
+                    /* 관리대상) SecurityContextHolder -> SecurityContext -> Authentication -> 사용자 인증 정보
+                     *
+                     * SecurityContextHolder 내부에 있는 값을 통해 인증여부를 알 수 있음
+                     * SecurityContextHolder.getContext().getAuthentication() == null -> 인증상태 X */
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+        } catch (Exception e) {
+            request.setAttribute("JwtFilterException", e);
+        }finally {
             filterChain.doFilter(request, response);
-            return;
         }
 
-        jwtToken = authHeader.substring(7); // Bearer 접두사 제거하여 토큰추출
-        userEmail = jwtService.extractUsername(jwtToken);
-
-
-        //  아직 인증 상태가 아닐때 (null 이면 아직 인증 X)
-       if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-           UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail); // db 로부터 사용자정보 로드
-
-           if(jwtService.isTokenValid(jwtToken, userDetails)){
-
-               // 인증 객체(Authentication) 얻기
-               // 참고: UsernamePasswordAuthenticationToken (구현체) -> Authentication
-               UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                       userDetails,
-                       null,   // 현재 구현에서는 자격증명이 없기에 null로 넘김
-                       userDetails.getAuthorities() // 권한 넘김
-               );
-
-               // 참고: WebAuthenticationDetailsSource (구현체) -> AuthenticationDetailsSource
-               authToken.setDetails(
-                       new WebAuthenticationDetailsSource().buildDetails(request) // WebAuthenticationDetails 반환
-               );
-
-               /*
-               *  관리대상) SecurityContextHolder -> SecurityContext -> Authentication -> 사용자 인증 정보
-               *
-               * SecurityContextHolder 내부에 있는 값을 통해 인증여부를 알 수 있음
-               * SecurityContextHolder.getContext().getAuthentication() == null -> 인증상태 X
-               */
-               SecurityContextHolder.getContext().setAuthentication(authToken);
-           }
-       }
-
-       filterChain.doFilter(request, response);
     }
 }

b2435c0d4d4ef32235b58d06ba2028f7b5d2f072
TestSeung <sjlim589@hanmail.net>
2024-09-25 15:33:32 +0900

    Update(댓글): 유저인증 관련 #5
    유저인증관련데이터를 받고 응답하는
    Controller

--- src/main/java/project_1st_team03/dashboard/domain/comment/api/CommentController.java
+++ src/main/java/project_1st_team03/dashboard/domain/comment/api/CommentController.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import project_1st_team03.dashboard.domain.comment.application.CommentService;
 import project_1st_team03.dashboard.domain.comment.domain.Comment;
 import project_1st_team03.dashboard.domain.comment.dto.CommentsRequest;
 import project_1st_team03.dashboard.domain.comment.dto.CommentsResponse;
+import project_1st_team03.dashboard.global.security.MemberDetails;
 
 import java.util.HashMap;
 import java.util.List;
@@ -24,35 +26,43 @@ public class CommentController {
 
     @PostMapping("/comments")
     public ResponseEntity<String> createComment(
+            @AuthenticationPrincipal MemberDetails memberDetails,
             @RequestBody CommentsRequest request) {
 
-        commentService.createComments(request);
+        commentService.createComments(memberDetails,request);
         return ResponseEntity.ok("댓글이 성공적으로 작성되었습니다.");
     }
 
+
+    /**
+     * list로 반환 시 key가 0부터시작해서 자동으로 넣어졌다.
+     * react 연동 시 key값을 comments로 받기 때문에 map으로 재반환해주었다.
+     */
     @GetMapping("/comments")
     public ResponseEntity<Map<String,Object>> getAllComments() {
+        //Question 질문입니다~
         List<CommentsResponse> comments = commentService.getAllComment();
-        //comments 필드로 응답.
         Map<String, Object> response = new HashMap<>();
         response.put("comments", comments);
-
         return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/comments/{comment_id}")
     public ResponseEntity<String> deleteCommentByPathId(
+            @AuthenticationPrincipal MemberDetails memberDetails,
             @PathVariable("comment_id") Long id){
-        commentService.deleteComment(id);
+
+        commentService.deleteComment(memberDetails,id);
         return ResponseEntity.ok("댓글이 삭제되었습니다.");
     }
 
     @PutMapping("/comments/{comment_id}")
     public ResponseEntity<String> updateComment(
+            @AuthenticationPrincipal MemberDetails memberDetails,
             @PathVariable("comment_id") Long id,
             @RequestBody CommentsRequest commentRequest){
-
-        commentService.updateComment(id,commentRequest);
+    
+        commentService.updateComment(memberDetails,id,commentRequest);
         return ResponseEntity.ok("댓글이 성공적으로 수정되었습니다.");
     }
 }
--- src/main/java/project_1st_team03/dashboard/domain/comment/application/CommentService.java
+++ src/main/java/project_1st_team03/dashboard/domain/comment/application/CommentService.java
@@ -3,14 +3,17 @@ package project_1st_team03.dashboard.domain.comment.application;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import project_1st_team03.dashboard.domain.comment.dao.CommentRepository;
 import project_1st_team03.dashboard.domain.comment.domain.Comment;
 import project_1st_team03.dashboard.domain.comment.dto.CommentsRequest;
 import project_1st_team03.dashboard.domain.comment.dto.CommentsResponse;
+import project_1st_team03.dashboard.domain.comment.exception.CommentException;
 import project_1st_team03.dashboard.domain.member.dao.MemberRepository;
 import project_1st_team03.dashboard.domain.member.domain.Member;
 import project_1st_team03.dashboard.domain.post.dao.PostRepository;
-import project_1st_team03.dashboard.domain.post.domain.Post;
+import project_1st_team03.dashboard.global.exception.ErrorCode;
+import project_1st_team03.dashboard.global.security.MemberDetails;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,18 +25,19 @@ public final CommentRepository commentRepository;
 public final MemberRepository memberRepository;
 public final PostRepository postRepository;
 
-    public void createComments(CommentsRequest commentsRequest) {
+    public void createComments(MemberDetails memberDetails,
+            CommentsRequest commentsRequest) {
 
         //Post post = postRepository.findById(commentsRequest.getPostId())
         //        .orElseThrow(() -> new EntityNotFoundException("Post not found"));
 
-      //  long author = Long.parseLong( commentsRequest.getAuthor());
-       // Member member = memberRepository.findById(author)
-        //        .orElseThrow(() -> new EntityNotFoundException("Member not found"));
+        String author =  memberDetails.getUsername();
+        Member member = memberRepository.findByEmail(author)
+                .orElseThrow(EntityNotFoundException::new);
 
         Comment comment = Comment.builder()
                 .content(commentsRequest.getContent())
-                .member(null)
+                .member(member)
                 .post(null)
                 .build();
         commentRepository.save(comment);
@@ -46,7 +50,7 @@ public final PostRepository postRepository;
             CommentsResponse.builder()
                     .id(comment.getId())
                     .content(comment.getContent())
-                    .memberId(1L)
+                    .memberId(comment.getMember().getId())
                     .postId(1L)
                     .createdAt(comment.getCreatedAt())
                     .modifiedAt(comment.getModifiedAt())
@@ -56,13 +60,26 @@ public final PostRepository postRepository;
 
     }
 
-    public void deleteComment(Long commentId) {
+    @Transactional
+    public void deleteComment(MemberDetails memberDetails,Long commentId) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new CommentException(ErrorCode.NOT_FOUND_COMMENT));
+
+        if (!comment.getMember().getEmail().equals(memberDetails.getUsername())) {
+            throw new CommentException(ErrorCode.UNAUTHORIZED_EDIT_COMMENT_ATTEMPT);
+        }
         commentRepository.deleteById(commentId);
     }
 
-    public void updateComment(Long id, CommentsRequest commentRequest) {
+    @Transactional
+    public void updateComment(MemberDetails memberDetails,
+                              Long id, CommentsRequest commentRequest) {
         Comment comment = commentRepository.findById(id).orElseThrow(
-                ()-> new RuntimeException("Comment not found"));
+                ()-> new CommentException(ErrorCode.NOT_FOUND_COMMENT));
+
+        if (!comment.getMember().getEmail().equals(memberDetails.getUsername())) {
+            throw new  CommentException(ErrorCode.UNAUTHORIZED_EDIT_COMMENT_ATTEMPT);
+        }
         comment.setContent(commentRequest.getContent());
         commentRepository.save(comment);
 

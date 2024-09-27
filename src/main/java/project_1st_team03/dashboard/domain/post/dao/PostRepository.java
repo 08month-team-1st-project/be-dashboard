d74c343d0efe0839f573703cef5eb1d3a22bfb5b
DokyungYou <willow74rr@gmail.com>
2024-09-27 15:17:15 +0900

    Refactor: 게시글 삭제 구현에 따른 조회쿼리 수정 - #4

--- src/main/java/project_1st_team03/dashboard/domain/post/dao/PostRepository.java
+++ src/main/java/project_1st_team03/dashboard/domain/post/dao/PostRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import project_1st_team03.dashboard.domain.post.domain.Post;
 
@@ -11,7 +12,7 @@ import project_1st_team03.dashboard.domain.post.domain.Post;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
 
-    @Query(value = "select p from Post p join fetch p.member"
+    @Query(value = "select p from Post p join fetch p.member where p.status = 'NORMAL'"
             //,countQuery = "select count(p) from Post p join p.member"
             // 본래는 페치조인 jpql 작성 시 카운트 쿼리를 별도로 작성했어야했었음
     )
@@ -19,6 +20,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
 
     // Question where 조건문이 있을 때는 별도로 count 쿼리가 나가지 않는 이유
-    @Query(value = "select p from Post p join fetch p.member m where m.email = :email")
-    Page<Post> findPostPageByEmail(String email, Pageable pageable);
+    @Query(value = "select p from Post p join fetch p.member m where m.email = :email and p.status = 'NORMAL'")
+    Page<Post> findPostPageByEmail(@Param("email") String email, Pageable pageable);
 }
